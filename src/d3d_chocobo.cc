cda6eeb0a733901713f4cb3c1488baccca1c1b2d
Jake Loo <jake@bebo.com>
2018-03-07 05:12:06 -0800

    Include sample code for d3d -> gl (commented out)

--- src/d3d_chocobo.cc
+++ src/d3d_chocobo.cc
@@ -16,7 +16,7 @@ void d3d11_create_device() {
   GST_INFO("d3d11 create device");
 
   ID3D11Device *device;
-  ID3D11Resource *resource;
+  ID3D11Texture2D *texture;
   ID3D11DeviceContext *context;
 //  IDXGIAdapter *adapter;
 
@@ -35,11 +35,41 @@ void d3d11_create_device() {
       (unsigned int) level_used);
 
   hr = device->OpenSharedResource(handle,
-      __uuidof(ID3D11Texture2D), (void**)&resource);
+      __uuidof(ID3D11Texture2D), (void**)&texture);
 
 #if 0
-  ID3D11Texture2D* shared_texture = (ID3D11Texture2D*)1;
-  wglDXSetResourceShareHandleNV(shared_texture, handle);
+  HANDLE gl_handleD3D = wglDXOpenDeviceNV(device);
+  HANDLE gl_handles[2];
+  GLuint gl_names[2];
+
+  BOOL success = wglDXSetResourceShareHandleNV(texture, handle);
+
+  glGenTextures(1, gl_names);
+
+  gl_handles[0] = wglDXRegisterObjectNV(gl_handleD3D, texture,
+      gl_names[0],
+      GL_TEXTURE_2D,
+      WGL_ACCESS_READ_WRITE_NV);
+
+  glBindTexture(GL_TEXTURE_2D, gl_names[0]);
+
+  //no filtering
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+  glGenerateMipmap(GL_TEXTURE_2D);
+
+  GLuint fbo;
+  glGenFramebuffers(1, &fbo);
+
+  // attach the Direct3D buffers to an FBO
+  glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, 
+      GL_TEXTURE_2D, gl_names[0], 0);
+
+  GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
 #endif
 
   GST_WARNING("JAKE OpenSharedResource HR: 0x%08x", hr);
