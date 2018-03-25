97140e570e12b24bffde0d95c624cbac008a0257
RowanMeara <rrm89@cornell.edu>
2018-03-24 18:40:46 -0700

    Remove completed todo.

--- src/game_capture.cc
+++ src/game_capture.cc
@@ -1148,7 +1148,6 @@ gboolean game_capture_tick(void * data) {
 gboolean game_capture_stop(void *data) {
   struct game_capture *gc = (game_capture*) data;
   stop_capture(gc);
-  // TODO: Causes crashes.
   g_free(gc);
   return TRUE;
 }
